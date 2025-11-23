8ab009449bbd15b1c70e3f1452e7b49dc5eb36b3
MariaPolania <mpaula.polaniau@gmail.com>
2025-11-23 11:48:22 -0500

    Ajuste de URL en config.py

--- App/config.py
+++ App/config.py
@@ -12,7 +12,7 @@ class Settings:
     DEBUG: bool = True
     ICFES_API_URL: str = os.getenv(
         "ICFES_API_URL",
-        "http://127.0.0.1:8000/predict",  # valor por defecto
+        "backend-production-aafd.up.railway.app/docs/predict",  # valor por defecto
     )
 
 settings = Settings()
