93403db465cf5b0cc1b7b9c0eb5b465dc2e58815
MariaPolania <mpaula.polaniau@gmail.com>
2025-11-23 17:08:29 -0500

    Ajuste de URL para despliegue local

--- App/config.py
+++ App/config.py
@@ -12,8 +12,8 @@ class Settings:
     DEBUG: bool = True
     ICFES_API_URL: str = os.getenv(
         "ICFES_API_URL",
-    #    "http://127.0.0.1:8000/predict", #valor por defecto para el despliegue local
-        "http://backend-production-aafd.up.railway.app/predict",  # valor desplegado en railway
+        "http://127.0.0.1:8000/predict", #valor por defecto para el despliegue local
+    #    "http://backend-production-aafd.up.railway.app/predict",  # valor desplegado en railway
     )
 
 settings = Settings()
