33e81e9e571d21b7400edeab95415699e5cbea11
MariaPolania <mpaula.polaniau@gmail.com>
2025-11-23 11:08:40 -0500

    Ajuste en el llamado de páginas en app.py

--- App/app.py
+++ App/app.py
@@ -2,7 +2,7 @@ from dash import Dash, html, dcc
 import dash
 import dash_bootstrap_components as dbc
 
-app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, use_pages=True, pages_folder="Pages", external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.title = "SaberInsight"
 
 app.layout = html.Div([
