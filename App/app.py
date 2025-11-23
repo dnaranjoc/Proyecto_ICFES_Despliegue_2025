20c16455adf95ff301cb585089b9ed435edb2525
MariaPolania <mpaula.polaniau@gmail.com>
2025-11-23 11:31:01 -0500

    Ajuste de puertos

--- App/app.py
+++ App/app.py
@@ -25,5 +25,12 @@ app.layout = html.Div([
     dash.page_container
 ])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 8050))  
+    app.run_server(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
